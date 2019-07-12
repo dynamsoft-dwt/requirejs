f5b16371471d4082a6bbc05c48cad16b56335477
Tom Kent <Tom@dynamsoft.com>
2019-07-12 17:13:12 +0800

    updated to v15.0.3 of DWT

--- README.md
+++ README.md
@@ -1,38 +1,53 @@
 # Dynamic Web TWAIN with RequireJS
 
-To use Dynamic Web TWAIN with RequireJS, add following code to **main.js**:
+To use Dynamic Web TWAIN with RequireJS, steps are
+
+1. Add following code to **main.js**:
 
 ```javascript
 require.config({
     paths: {
-        "dwt": "https://www.dynamsoft.com/library/dwt/dynamsoft.webtwain.min",
-    },
-    shim: {
-        'dwt': {　　　　　　　　
-            exports: 'Dynamsoft'　　　　　　
-        }
+        "dwt": "https://unpkg.com/dwt/dist/dynamsoft.webtwain.min",
     }
 });
 ```
-Dynamically load Web TWAIN module in **main.js**:
+2. Dynamically load Dynamic Web TWAIN module in **main.js**:
 
 ```javascript
 // Load Dynamic Web TWAIN.
 requirejs(["dwt"], function (module) {
-    Dynamsoft = module;
-    initializeContainer(Dynamsoft);
+    initializeContainer(module);
 });
+```
+3. Config and create an instance of Dynamic Web TWAIN to scan. Don't forget to add a good (trial/full) license key (replace **A-Valid-Product-Key**).
 
+```javascript
 // Initialize the container when Dynamic Web TWAIN is ready.
 function initializeContainer(Dynamsoft) {
-    Dynamsoft.WebTwainEnv.CreateDWTObject(docContainer.id, function(obj){
-        dwtObject = obj;
-        dwtObject.Width = 480;
-        dwtObject.Height = 640;
-    }, function(errorString){
+    Dynamsoft.WebTwainEnv.AutoLoad = false;
+    /**
+     * In order to use the full version, do the following
+     * 1. Change Dynamsoft.WebTwainEnv.Trial to false
+     * 2. Replace A-Valid-Product-Key with a full version key
+     * 3. Change Dynamsoft.WebTwainEnv.ResourcesPath to point to the full version 
+     *    resource files that you obtain after purchasing a key
+     */
+    Dynamsoft.WebTwainEnv.Trial = true;
+    Dynamsoft.WebTwainEnv.ProductKey = "A-Valid-Product-Key";
+    //Dynamsoft.WebTwainEnv.ResourcesPath = "https://tst.dynamsoft.com/libs/dwt/15.0";
+
+    // Create a document container
+    var docContainer = document.createElement('div');
+    docContainer.id = "docContainer";
+    document.body.appendChild(docContainer);
+    
+    Dynamsoft.WebTwainEnv.CreateDWTObject(docContainer.id, function (obj) {
+        DWObject = obj;
+    }, function (errorString) {
         console.log(errorString);
     });
 }
+
 ```
 
 ## Blog
--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,37 @@
 require.config({
     paths: {
-        "dwt": "https://www.dynamsoft.com/library/dwt/dynamsoft.webtwain.min",
-    },
-    shim: {
-        'dwt': {　　　　　　　　
-            exports: 'Dynamsoft'　　　　　　
-        }
+        "dwt": "https://unpkg.com/dwt/dist/dynamsoft.webtwain.min",
     }
 });
 
-var dwtObject = null;
+var DWObject = null;
 
 // Load Dynamic Web TWAIN.
 requirejs(["dwt"], function (module) {
     initializeContainer(module);
 });
 
-// Create a document container
-var docContainer = document.createElement('div');
-docContainer.id = "docContainer";
-document.body.appendChild(docContainer);
-
 // Initialize the container when Dynamic Web TWAIN is ready.
 function initializeContainer(Dynamsoft) {
+    Dynamsoft.WebTwainEnv.AutoLoad = false;
+    /**
+     * In order to use the full version, do the following
+     * 1. Change Dynamsoft.WebTwainEnv.Trial to false
+     * 2. Replace A-Valid-Product-Key with a full version key
+     * 3. Change Dynamsoft.WebTwainEnv.ResourcesPath to point to the full version 
+     *    resource files that you obtain after purchasing a key
+     */
+    Dynamsoft.WebTwainEnv.Trial = true;
+    Dynamsoft.WebTwainEnv.ProductKey = "A-Valid-Product-Key";
+    //Dynamsoft.WebTwainEnv.ResourcesPath = "https://tst.dynamsoft.com/libs/dwt/15.0";
+
+    // Create a document container
+    var docContainer = document.createElement('div');
+    docContainer.id = "docContainer";
+    document.body.appendChild(docContainer);
+    
     Dynamsoft.WebTwainEnv.CreateDWTObject(docContainer.id, function (obj) {
-        dwtObject = obj;
-        dwtObject.Width = 480;
-        dwtObject.Height = 640;
+        DWObject = obj;
     }, function (errorString) {
         console.log(errorString);
     });
@@ -35,21 +40,21 @@ function initializeContainer(Dynamsoft) {
 // Scan documents when clicking a button.
 var scanButton = document.getElementById('scanButton');
 scanButton.onclick = function () {
-    if (dwtObject == null) {
+    if (DWObject == null) {
         alert("Dynamic Web TWAIN is not ready!");
         return;
     }
 
-    dwtObject.IfDisableSourceAfterAcquire = true;
-    var bSelected = dwtObject.SelectSource();
+    DWObject.IfDisableSourceAfterAcquire = true;
+    var bSelected = DWObject.SelectSource();
 
     if (bSelected) {
         var successCallback, failCallback;
         successCallback = failCallback = function () {
-            dwtObject.CloseSource();
+            DWObject.CloseSource();
         };
 
-        dwtObject.OpenSource();
-        dwtObject.AcquireImage(successCallback, failCallback);
+        DWObject.OpenSource();
+        DWObject.AcquireImage(successCallback, failCallback);
     }
 };
